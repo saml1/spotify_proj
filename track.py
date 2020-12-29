3943f2ac8b425871b126e05d7ccf2ec23fa3b051
saml1 <sam.lipschitz1@gmail.com>
2020-12-29 15:07:10 -0500

    working

--- track.py
+++ track.py
@@ -1,7 +1,8 @@
 class Track:
-    def __init__(self, name, album, artist, length, year):
+    def __init__(self, name, album, artist, length):
         self.name = name
         self.album = album
         self.artist = artist
+        # length is in milliseconds
         self.length = length
-        self.year = year
+        # self.year = year - removed this bc data isn't in spotify track object and it doesnt matter
